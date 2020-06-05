4e8932f4e8334ad6a88a2a698db234cc797f8007
--replace-all <altmurzilka@gmail.com>
2020-06-05 13:27:33 +0600

    8

---
+++ acmp.ru/Арифметика.py
@@ -0,0 +1,11 @@
+fin  = open("input.txt")
+fout = open("output.txt","w")
+  
+a = [int(x) for x in fin.read().split()]
+if a[0] * a[1] == a[2]:
+    fout.write("YES")
+else:
+    fout.write("NO")
+  
+fin.close()
+fout.close()
